53e3f51727dab13ea967d0e176d6104d3068c07d
Jameson Haas <jdhaas59@gmail.com>
2023-08-27 20:49:45 -0400

    remove unused

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
 import Nav from './nav';
-import Toast from './toast';
 import { Suspense } from 'react';
 
 export const metadata = {
