d9d69015dbf53dce0b91557e92663f98dc70aad8
Jameson Haas <jdhaas59@gmail.com>
2023-08-27 22:30:21 -0400

    comment out search

--- app/home/standings.tsx
+++ app/home/standings.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 export default function Standings() {
   return (
     <Card className="flex-1 p-4 m-1">
-    {/* Main section content */}
     <Title>Standings</Title>
     <Text className='text-center'>Season Starts: Thu, Sep 7, 2023</Text>
   </Card>
